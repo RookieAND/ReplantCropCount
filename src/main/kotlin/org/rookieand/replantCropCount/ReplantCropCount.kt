2cb32b091a22cb9fce84fd8f2c0c6ecc841896f2
RookieAND <gwangin1999@naver.com>
2024-04-01 23:46:15 +0900

    fix: main 경로 수정 및 config 관련 메서드 제거

--- src/main/kotlin/org/rookieand/replantCropCount/ReplantCropCount.kt
+++ src/main/kotlin/org/rookieand/replantCropCount/ReplantCropCount.kt
@@ -1,17 +1,17 @@
 package org.rookieand.replantCropCount
 
+import net.kyori.adventure.text.Component
 import org.bukkit.plugin.java.JavaPlugin
 
 class ReplantCropCount: JavaPlugin() {
     override fun onEnable() {
         // setup
-        saveDefaultConfig()
         logger.info("test")
+        server.consoleSender.sendMessage(Component.text("plugin is enabled."))
     }
 
     override fun onDisable() {
         // setup
-        saveConfig()
+        logger.info("test")
     }
-
 }
