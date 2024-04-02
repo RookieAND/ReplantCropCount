8f7705bd339f9d78447f6455ae88991dbdac97a6
RookieAND <gwangin1999@naver.com>
2024-04-02 22:18:48 +0900

    chore: 코드 포맷팅 1차 적용

--- src/main/kotlin/org/rookieand/replantCropCount/ReplantCropCountPlugin.kt
+++ src/main/kotlin/org/rookieand/replantCropCount/ReplantCropCountPlugin.kt
@@ -3,7 +3,7 @@ package org.rookieand.replantCropCount
 import net.kyori.adventure.text.Component
 import org.bukkit.plugin.java.JavaPlugin
 
-class ReplantCropCountPlugin: JavaPlugin() {
+class ReplantCropCountPlugin : JavaPlugin() {
     override fun onEnable() {
         logger.info("test")
         server.consoleSender.sendMessage(Component.text("plugin is enabled."))
--- src/main/kotlin/org/rookieand/replantCropCount/listener/BlockChangesListener.kt
+++ src/main/kotlin/org/rookieand/replantCropCount/listener/BlockChangesListener.kt
@@ -10,45 +10,53 @@ import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.inventory.PlayerInventory
 
 class BlockChangesListener : Listener {
-    private val cropBlockSet = setOf<Material>(Material.WHEAT, Material.CARROT, Material.POTATO, Material.NETHER_WART, Material.BEETROOTS)
-    private val cropSeedMap = mapOf<Material, Material>(Material.WHEAT to Material.WHEAT_SEEDS, Material.POTATO to Material.POTATOES, Material.CARROT to Material.CARROTS, Material.NETHER_WART to Material.NETHER_WART, Material.BEETROOTS to Material.BEETROOT_SEEDS);
-    private fun isFullyGrown (blockData: BlockData): Boolean {
-        return if (blockData is Ageable) blockData.maximumAge == blockData.age else false;
+    private val cropBlockSet =
+        setOf<Material>(Material.WHEAT, Material.CARROT, Material.POTATO, Material.NETHER_WART, Material.BEETROOTS)
+    private val cropSeedMap = mapOf<Material, Material>(
+        Material.WHEAT to Material.WHEAT_SEEDS,
+        Material.POTATO to Material.POTATOES,
+        Material.CARROT to Material.CARROTS,
+        Material.NETHER_WART to Material.NETHER_WART,
+        Material.BEETROOTS to Material.BEETROOT_SEEDS
+    )
+
+    private fun isFullyGrown(blockData: BlockData): Boolean {
+        return if (blockData is Ageable) blockData.maximumAge == blockData.age else false
     }
 
     private fun removeSeeds(cropType: Material, playerInventory: PlayerInventory): Boolean {
-        if (!cropBlockSet.contains(cropType)) return false;
+        if (!cropBlockSet.contains(cropType)) return false
 
-        val needRemovedSeeds = cropSeedMap[cropType] ?: return false;
-        if (!playerInventory.contains(needRemovedSeeds)) return false;
+        val needRemovedSeeds = cropSeedMap[cropType] ?: return false
+        if (!playerInventory.contains(needRemovedSeeds)) return false
 
-        val inventoryIterator = playerInventory.iterator();
+        val inventoryIterator = playerInventory.iterator()
 
         while (inventoryIterator.hasNext()) {
             val item = inventoryIterator.next()
             if (item.type === needRemovedSeeds) {
-                val slotIndex = inventoryIterator.nextIndex();
+                val slotIndex = inventoryIterator.nextIndex()
                 if (item.amount == 1) {
-                    playerInventory.setItem(slotIndex, null);
+                    playerInventory.setItem(slotIndex, null)
                 } else {
-                    item.amount -= 1;
-                    playerInventory.setItem(slotIndex, item);
+                    item.amount -= 1
+                    playerInventory.setItem(slotIndex, item)
                 }
-                break;
+                break
             }
         }
 
-        return true;
+        return true
     }
 
     @EventHandler
     fun onBreakCropBlock(event: BlockBreakEvent) {
         val player = event.player
-        if (player.gameMode !== GameMode.SURVIVAL) return;
+        if (player.gameMode !== GameMode.SURVIVAL) return
 
-        val brokenBlockData = event.block.blockData;
+        val brokenBlockData = event.block.blockData
         if (isFullyGrown(brokenBlockData) and cropBlockSet.contains(brokenBlockData.material)) {
-            val hasEnoughSeed = removeSeeds(brokenBlockData.material, player.inventory);
+            val hasEnoughSeed = removeSeeds(brokenBlockData.material, player.inventory)
         }
     }
 }
