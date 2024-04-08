803b4b72befa8916fb140c701e02bb7cfb520825
RookieAND <gwangin1999@naver.com>
2024-04-09 02:11:06 +0900

    feat: PlayerManager Object 1차 작성

---
+++ src/main/kotlin/org/rookieand/replantCropCount/manager/PlayerManager.kt
@@ -0,0 +1,54 @@
+package org.rookieand.replantCropCount.manager
+
+import org.bukkit.entity.Player
+
+object PlayerManager {
+
+    private val playerCountMap = hashMapOf<Player, Int>()
+
+    fun registerPlayer(player: Player) {
+        playerCountMap[player] = 0
+    }
+
+    fun getReplantCount(player: Player): Int {
+        val leftReplantAmount = playerCountMap[player]
+
+        if (leftReplantAmount === null) {
+            registerPlayer(player)
+            return 0
+        }
+
+        return leftReplantAmount
+    }
+
+    fun addReplantCount(player: Player, count: Int) {
+        val leftReplantAmount = playerCountMap[player]
+
+        if (leftReplantAmount === null) {
+            registerPlayer(player)
+            return
+        }
+
+        playerCountMap[player] = leftReplantAmount + count
+    }
+
+    fun takeReplantCount(player: Player, count: Int) {
+        val leftReplantAmount = playerCountMap[player]
+
+        if (leftReplantAmount === null) {
+            registerPlayer(player)
+            return
+        }
+
+        playerCountMap[player] = leftReplantAmount - count
+    }
+
+    fun setReplantCount(player: Player, count: Int) {
+        if (playerCountMap[player] === null) {
+            registerPlayer(player)
+            return
+        }
+
+        playerCountMap[player] = count
+    }
+}
